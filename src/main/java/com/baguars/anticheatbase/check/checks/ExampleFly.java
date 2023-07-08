14527060bde14d2e89e2ff0b49eb05af2714fe4e
MCGenerals <138998996+mcgenerals@users.noreply.github.com>
2023-07-09 01:39:08 +0900

    Added Description.

--- src/main/java/com/baguars/anticheatbase/check/checks/ExampleFly.java
+++ src/main/java/com/baguars/anticheatbase/check/checks/ExampleFly.java
@@ -8,11 +8,17 @@ public class ExampleFly extends Check {
     public ExampleFly(String name, String type) {
         super(name, type);
     }
+    /*
+        This check is easly to false. 
+        This is just "example".
+    */
 
     @EventHandler
-    public void onMove(CustomMoveEvent e){
-        if( e.getServerAirTick() < 12 )return;
-        if( e.getTo().getY() >= e.getFrom().getY() ){
+    public void onMove(CustomMoveEvent e){ 
+        if( e.getServerAirTick() < 12 )return; 
+        // you can change it from 12 to 7. but I will suggest 12 because it may false when jummping
+        if( e.getTo().getY() >= e.getFrom().getY() ){ 
+            // if they are NOT going down. they are cheating.
             flag( e.getPlayer() , "airtick=" + e.getServerAirTick() );
         }
     }
